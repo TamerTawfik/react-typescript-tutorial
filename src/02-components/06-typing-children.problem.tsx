7ba7e94eb704398abe44487eaf98c08dc73c2395
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-05-19 04:16:48 +0200

    Solved: Problem 06

--- src/02-components/06-typing-children.problem.tsx
+++ src/02-components/06-typing-children.problem.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-export const Button = (props: {}) => {
-  return <button>{props.children}</button>;
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const Button = ({ children }: Props) => {
+  return <button>{children}</button>;
 };
 
 const Parent = () => {
   return (
     <>
-      {/* @ts-expect-error */}
       <Button></Button>
       <Button>Hello world!</Button>
     </>
